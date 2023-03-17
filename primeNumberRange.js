db917b38d67a7fcf2712d6686051889a93bcb7ca
Admin <akshatchawla1997@gmail.com>
2023-03-17 18:31:06 +0530

    prime numbers within a range

---
+++ primeNumberRange.js
@@ -0,0 +1,15 @@
+const primeNumberRange =(to, from)=>{
+    for (let i = to; i<=from;i++){
+        let count = 0;
+    for (let j=2;j<i/2;j++){
+        if(i%j==0){
+            count=1;
+            break;
+        }
+    }
+    count==0?console.log(`${i} is prime number`):console.log(`${i} is not a prime number`)
+
+    }
+}
+
+primeNumberRange(5,25);
