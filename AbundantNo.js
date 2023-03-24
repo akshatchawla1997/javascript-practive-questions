1e6828d363fa794413139deb5d70a95fada43486
Admin <akshatchawla1997@gmail.com>
2023-03-25 01:22:27 +0530

    Abundant number added to the  list

---
+++ AbundantNo.js
@@ -0,0 +1,9 @@
+const abundantNo = (number)=>{
+    let sum = 0,n=number;
+    for (i=1;i<=number/2;i++){
+        (number%i===0)?sum+=i:"";
+    }
+    (sum>n)?console.log(`${n} is Abundant Number`):console.log(`${n} is not aAbundant Number`)
+}
+abundantNo(10);
+abundantNo(12);
