df0249b825fa7109f5d1aec06382a050b3e7ed5f
Admin <akshatchawla1997@gmail.com>
2023-03-24 23:45:17 +0530

    automorphic number added to the list

---
+++ Automorphicnumber.js
@@ -0,0 +1,18 @@
+const automorphic = (number)=>{
+    let n=number;
+    let square = n*n;
+    (checknum(n,square))?console.log(number,"is automorphic number"):console.log(number,"is not automorphic number")
+}
+
+const checknum =(n,sq)=>{
+    while(n>0){
+        if(n%10!=sq%10){
+            return false;
+        }
+        n=~~(n/10)
+        sq=~~(sq/10)
+    }
+    return true
+}
+automorphic(20);
+automorphic(25);
