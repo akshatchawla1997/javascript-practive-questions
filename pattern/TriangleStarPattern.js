f27da6471c2a287ddc7c4fc2a81a63bc71dfa4ec
emp0107 <115693833+emp0107@users.noreply.github.com>
2023-11-02 23:49:07 +0530

    triangle pattern added to the repository

---
+++ pattern/TriangleStarPattern.js
@@ -0,0 +1,19 @@
+// Triangle Star pattern in javascript
+
+// *
+// **
+// ***
+// ****
+
+const TrianglePattern = (n=>{
+    for (let i=1;i<=n;i++){
+        let row = "";
+        for (let j=1;j<=i;j++) {
+            row += '*'
+        }
+
+        console.log(row)
+    }
+})
+
+TrianglePattern(5)
