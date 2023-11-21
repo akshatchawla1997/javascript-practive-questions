1a5ba358e3c0313772e4295b27a0ca0ff3ed9392
emp0107 <115693833+emp0107@users.noreply.github.com>
2023-11-21 23:48:33 +0530

    string Anagram code added

---
+++ strings/stringAnagram.js
@@ -0,0 +1,18 @@
+// Check if two strings are Anagram or not
+
+// Anagram is a string where characters of one string is equal to characters of another string
+
+function anagramString(string1, string2){
+    if(string1.len != string2.len){
+        return 0
+    }else{
+        
+    }
+    // if(string1===string2){
+    //     return "the both strings are anagram string";
+    // }else{
+    //     return "the both strings are not anagram string";
+    // }
+}
+
+console.log(anagramString("palindromes","palindrome"));
