54765aa35358bb984e841a9ff0e596f7b7718106
emp0107 <115693833+emp0107@users.noreply.github.com>
2023-11-21 21:18:26 +0530

    non repeating character array in a string

---
+++ strings/nonRepeatingChar.js
@@ -0,0 +1,21 @@
+// Find non-repeating characters in a string
+
+function charfrequency(string){
+    let frequency = {}
+    for (let char of string){
+        if(frequency[char]){
+            frequency[char]++
+        }else{
+            frequency[char]=1;
+        }
+    }
+    let nonRepeatingChar = []
+    for (let char in frequency){
+        if(frequency[char]===1){
+            nonRepeatingChar.push(char)
+        }
+    }
+    return nonRepeatingChar;
+}
+const str = "akshat chawla"
+console.log(charfrequency(str))
